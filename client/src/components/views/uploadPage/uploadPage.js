4ae6bb5dcebc442051da8f07fd84c2f68bc7117f
davin <dizqar@gmail.com>
2020-03-03 14:02:40 +0700

    adding react-dropzone and uploadpage.js

--- client/src/components/views/uploadPage/uploadPage.js
+++ client/src/components/views/uploadPage/uploadPage.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {Typography, Button, Form, message, Input, icon} from 'antd';
+import Dropzone from 'react-dropzone';
+
+const { Title} = Typography;
+const {TextArea} = Input;
 
 function uploadPage() {
     return (
-        <div>
+        <div style={{maxWidth: '700px', margin: '2rem auto'}}>
+            <div style={{textAlign: 'center', marginBottom:'2rem'}}>
+                <Title level={2}>Upload Video</Title>
+            </div>
+            <Form>
+                <div style={{display:'flex', justifyContent:'space-between'}}>
+                <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                    {({getRootProps, getInputProps}) => (
+                        <section>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>Drag 'n' drop some files here, or click to select files</p>
+                        </div>
+                        </section>
+                    )}
+                </Dropzone>
+                </div>
+            </Form>
             
         </div>
     )
