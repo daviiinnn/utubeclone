064bcbace34386c132346de4af52b06b3e2a0132
davin <dizqar@gmail.com>
2020-03-04 09:33:45 +0700

    adding uploadvideo page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import LoginPage from './pages/LoginPage/loginPage.component';
 import RegisterPage from './pages/RegisterPage/registerPage.component';
 import NavBar from './components/navbar/navbar.component';
 import Footer from './components/footer/footer.component';
+import uploadvideopage from './pages/uploadvideoPage/uploadvideopage.component';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/upload" component={Auth(uploadvideopage, true)} />
         </Switch>
       </div>
       <Footer />
--- client/src/pages/uploadvideoPage/uploadvideopage.component.js
+++ client/src/pages/uploadvideoPage/uploadvideopage.component.js
@@ -4,6 +4,7 @@ import Dropzone from 'react-dropzone';
 import axios from 'axios';
 import {useSelector} from 'react-redux';
 import { response } from 'express';
+import { set } from 'mongoose';
 
 const {Title} = Typography;
 const {TextArea} = Input;
@@ -81,31 +82,108 @@ function uploadvideopage() {
         })
     
         }
-    return (
-        <div style={{maxWidth:'700px', margin:'2rem auto'}}>
-            <div style={{textAlign:'center', marginBottom:'2rem'}}>
-                <Title level={2}>
-                    Upload Video
-                </Title>
-            </div>
-            
-            <Form>
-            <Dropzone 
-                onDrop={acceptedFiles => console.log(acceptedFiles)}
-                multiple={false}
-                maxSize={800000000}>
-                {({getRootProps, getInputProps}) => (
-                    <section>
-                    <div {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <p>Drag and drop some files here, or click to select files</p>
+
+        const onDrop = (files) => {
+
+            let formData = new FormData();
+            const config = {
+                header: { 'content-type': 'multipart/form-data' }
+            }
+            console.log(files)
+            formData.append("file", files[0])
+    
+            axios.post('/api/video/uploadfiles', formData, config)
+                .then(response => {
+                    if (response.data.success) {
+    
+                        let variable = {
+                            filePath: response.data.filePath,
+                            fileName: response.data.fileName
+                        }
+                        setFilePath(response.data.filePath)
+    
+                        axios.post('/api/video/thumbnail', variable)
+                            .then(response => {
+                                if (response.data.success) {
+                                    setDuration(response.data.fileDuration)
+                                    setThumbnail(response.data.thumbsFilePath)
+                                } else {
+                                    alert('Failed to make the thumbnails');
+                                }
+                            })
+    
+    
+                    } else {
+                        alert('failed to save the video in server')
+                    }
+                })
+    
+        }
+        return (
+            <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+                <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                    <Title level={2} > Upload Video</Title>
+                </div>
+    
+                <Form onSubmit={onSubmit}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Dropzone
+                            onDrop={onDrop}
+                            multiple={false}
+                            maxSize={800000000}>
+                            {({ getRootProps, getInputProps }) => (
+                                <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                    {...getRootProps()}
+                                >
+                                    <input {...getInputProps()} />
+                                    <Icon type="plus" style={{ fontSize: '3rem' }} />
+    
+                                </div>
+                            )}
+                        </Dropzone>
+    
+                        {Thumbnail !== "" &&
+                            <div>
+                                <img src={`http://localhost:5000/${Thumbnail}`} alt="haha" />
+                            </div>
+                        }
                     </div>
-                    </section>
-                )}
-            </Dropzone>
-            </Form>
-        </div>
-    )
+    
+                    <br /><br />
+                    <label>Title</label>
+                    <Input
+                        onChange={handleChangeTitle}
+                        value={title}
+                    />
+                    <br /><br />
+                    <label>Description</label>
+                    <TextArea
+                        onChange={handleChangeDecsription}
+                        value={Description}
+                    />
+                    <br /><br />
+    
+                    <select onChange={handleChangeOne}>
+                        {Private.map((item, index) => (
+                            <option key={index} value={item.value}>{item.label}</option>
+                        ))}
+                    </select>
+                    <br /><br />
+    
+                    <select onChange={handleChangeTwo}>
+                        {Catogory.map((item, index) => (
+                            <option key={index} value={item.label}>{item.label}</option>
+                        ))}
+                    </select>
+                    <br /><br />
+    
+                    <Button type="primary" size="large" onClick={onSubmit}>
+                        Submit
+                </Button>
+    
+                </Form>
+            </div>
+        )
 }
 
 export default uploadvideopage
